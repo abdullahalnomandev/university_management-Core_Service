a4a52c0642fd43aa7a449268881386f1c3cb7c51
abdullahalnomanfb <abdullahalnoman1512@gmail.com>
2023-09-09 05:00:40 +0600

    added room

--- src/app/modules/building/building.routes.ts
+++ src/app/modules/building/building.routes.ts
@@ -1,11 +1,17 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import { BuildingController } from './building.controller';
+import { BuildingValidations } from './building.validation';
 
 
 const router = express.Router();
 
 router.post(
   '/',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN,ENUM_USER_ROLE.ADMIN),
+  validateRequest(BuildingValidations.create),
   BuildingController.insertIntoDB
 );
 router.get('/',BuildingController.getAllFromDB)
