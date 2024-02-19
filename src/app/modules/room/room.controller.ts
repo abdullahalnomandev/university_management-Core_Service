ccad56a5d6731bb9a21dbf97185d482e560add9d
abdullahalnomanfb <abdullahalnoman1512@gmail.com>
2024-02-19 08:08:58 +0600

    [Room has been added]

--- src/app/modules/room/room.controller.ts
+++ src/app/modules/room/room.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import httpStatus from "http-status";
 import catchAsync from "../../../shared/catchAsync";
+import pick from "../../../shared/pick";
 import sendResponse from "../../../shared/sendResponse";
+import { roomFilterableFields } from "./room.constant";
 import { RoomService } from "./room.service";
 
 
@@ -15,6 +17,19 @@ const insertIntoDB = catchAsync(async(req:Request, res:Response)=>{
     })
 })
 
+const getAllFromDB = catchAsync(async(req:Request, res:Response)=>{
+  const filters = pick(req.query, roomFilterableFields);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+  const result = await RoomService.getAllFromDB(filters, options); 
+     sendResponse(res,{
+        status:'success',
+        statusCode:httpStatus.OK,
+        message:"Room fetched successfully",
+        data:result
+    })
+})
+
 export const RoomController ={
-    insertIntoDB
+    insertIntoDB,
+    getAllFromDB
 }
--- src/app/modules/room/room.routes.ts
+++ src/app/modules/room/room.routes.ts
@@ -5,6 +5,7 @@ import { RoomController } from './room.controller';
 const router = express.Router();
 
 router.post('/',RoomController.insertIntoDB)
+router.get('/',RoomController.getAllFromDB)
 
 
 export const RoomRoutes = router;
